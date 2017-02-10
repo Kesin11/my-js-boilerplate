bc86f06cafd8580d6c3f87792a3e814c57024d05
Kenta Kase <kesin1202000@gmail.com>
2017-02-10 14:40:31 +0900

    vue-cli setup and install vuex

---
+++ assets/js/bundle.js
@@ -0,0 +1,88 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Base = function () {
+  function Base(name, version) {
+    _classCallCheck(this, Base);
+
+    this.name = name;
+    this.version = version;
+  }
+
+  _createClass(Base, [{
+    key: "get_name",
+    value: function get_name() {
+      return this.name;
+    }
+  }, {
+    key: "concat_name",
+    value: function concat_name(string) {
+      return this.name + string;
+    }
+  }]);
+
+  return Base;
+}();
+
+exports.default = Base;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _Base2 = require('./Base');
+
+var _Base3 = _interopRequireDefault(_Base2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Foo = function (_Base) {
+  _inherits(Foo, _Base);
+
+  function Foo(name, version) {
+    _classCallCheck(this, Foo);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Foo).call(this, name, version));
+
+    _this.name = name;
+    return _this;
+  }
+
+  _createClass(Foo, [{
+    key: 'get_name',
+    value: function get_name() {
+      return this.name;
+    }
+  }, {
+    key: 'concat_name',
+    value: function concat_name(string) {
+      return 'Foo: ' + this.name + string;
+    }
+  }]);
+
+  return Foo;
+}(_Base3.default);
+
+exports.default = Foo;
+
+},{"./Base":1}]},{},[1,2])
+//# sourceMappingURL=bundle.js.map
