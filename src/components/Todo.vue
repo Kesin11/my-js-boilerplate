7894555124e2ee895e03fa2f0950b005fd2e7931
Kenta Kase <kesin1202000@gmail.com>
2017-02-10 16:50:53 +0900

    init state rendering

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="todo">
+    <ul>
+      <li v-for="item in items">
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      items: [
+        { text: 'foo' },
+        { text: 'bar' }
+      ]
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Todo from '././components/Todo.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#todo',
+  render : h => h(Todo)
 })
